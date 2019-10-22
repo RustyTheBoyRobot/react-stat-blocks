e473e21a875262072c48e28f3b657fe647d7f816
Brad Stapleton <brad.stapleton@hudl.com>
2019-10-22 21:41:07 +0000

    Put the reference and the editable block side-by-side

--- src/js/components/StatBlockLibrary.jsx
+++ src/js/components/StatBlockLibrary.jsx
@@ -4,10 +4,19 @@ import StatBlockView from './StatBlockView.jsx';
 export default class StatBlockLibrary extends React.Component {
   render() {
     return (
-      <header>
-        <h1 id='pageTitle'>Stat Block Container</h1>
-        <StatBlockView/>
-      </header>
+      <div>
+        <header>
+          <h1 id='pageTitle'>Stat Block Library</h1>
+        </header>
+        <div id='library'>
+          <div className='libraryItem'>
+            <StatBlockView/>
+          </div>
+          <div className='libraryItem'>
+            <img src="https://raw.githubusercontent.com/Valloric/statblock5e/gh-pages/images/statblock.png"/>
+          </div>
+        </div>
+      </div>
     );
   }
 }
